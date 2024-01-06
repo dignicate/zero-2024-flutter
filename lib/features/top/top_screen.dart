785666db241bcc8ec5cae36801633e72faa2f023
Xinyiqi <shinichi_watanabe@dignicate.com>
2024-01-06 16:52:06 +0900

    wip, skeleton subscription ui and domain.

--- lib/features/top/top_screen.dart
+++ lib/features/top/top_screen.dart
@@ -16,6 +16,60 @@ class TopScreen extends HookConsumerWidget {
       return null;
     }, const []);
 
-    return Text(state.viewData?.title ?? "No data");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Top Page'),
+      ),
+      body: const Center(
+        child: _BodyWidget(),
+      ),
+    );
+  }
+}
+
+class _BodyWidget extends HookConsumerWidget {
+  const _BodyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // onPressed as empty function
+        _ButtonWidget(
+            text: 'Subscription test',
+            onPressed: () {
+              Navigator.of(context).pushNamed('/subscription');
+            },
+        ),
+        const SizedBox(height: 16.0),
+        // _ButtonWidget(text: '', onPressed: null),
+      ],
+    );
+  }
+}
+
+class _ButtonWidget extends StatelessWidget {
+  final String text;
+  final Function? onPressed;
+
+  const _ButtonWidget({Key? key, required this.text, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => onPressed?.call(),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white), // テキストを白色に設定
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black, // 背景を黒色に設定
+        fixedSize: Size(200, 50), // 固定幅を設定 (幅: 200, 高さ: 50)
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25), // 角の半径を高さの半分 (ここでは25) に設定
+        ),
+      ),
+    );
   }
 }
